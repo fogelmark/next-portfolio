339ce7a8b7c8a8c1a3c7b3cc11cdd44c9fdd4a63
fogelmark <108455196+fogelmark@users.noreply.github.com>
2024-07-05 11:05:33 +0200

    added new cv

--- components/intro.tsx
+++ components/intro.tsx
@@ -78,7 +78,7 @@ export function Intro() {
 
         <a
           className="borderBlack group flex cursor-pointer items-center gap-2 rounded-full bg-white px-7 py-3 outline-none transition hover:scale-110 focus:scale-110 active:scale-105 dark:bg-white/10"
-          href="/cv-alexander-fogelmark.pdf"
+          href="/cv-alexander-fogelmark-v2.pdf"
           download
         >
           Download CV{" "}
